4b3f7053cb0a6055dc83f4eb4333ae27a35eda68
Sejal <agarwalsejal230706.com>
2026-02-27 15:37:20 +0530

    seperate database folder

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
-import mongoose from "mongoose";
 
-const app = require("./app");
-
-const PORT = process.env.PORT || 5000;
+import express from "express";
+const port = process.env.PORT || 5000;
+const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${port}`);
 });
 
